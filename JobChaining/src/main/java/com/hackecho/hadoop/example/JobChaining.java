eacc2c61b11673fe86ef6afc3c604e2dcd91b2cd
zlmoment <zlmoment@gmail.com>
2014-05-01 10:34:34 -0500

    modify Job Chaining

---
+++ JobChaining/src/main/java/com/hackecho/hadoop/example/JobChaining.java
@@ -0,0 +1,13 @@
+package com.hackecho.hadoop.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class JobChaining 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
